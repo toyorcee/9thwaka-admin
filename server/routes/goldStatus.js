6d702b0b1524d8f998d8f98cb080a33345abe341
toyorcee <oluwatoyosiolaniyan@gmail.com>
2025-12-24 09:06:26 +0100

    Remove server from admin project

--- server/routes/goldStatus.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getGoldStatusStats } from "../controllers/goldStatusController.js";
-import { protect } from "../middleware/auth.js";
-
-const router = express.Router();
-
-router.get("/stats", protect, getGoldStatusStats);
-
-export default router;
